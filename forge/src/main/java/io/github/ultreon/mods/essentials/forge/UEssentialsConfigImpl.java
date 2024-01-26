c6f28f924b2845054015584bbc1c3b67d83fa019
XyperCode <qboiwastaken@gmail.com>
2024-01-26 14:52:54 +0100

    Bruh!

--- forge/src/main/java/io/github/ultreon/mods/essentials/forge/UEssentialsConfigImpl.java
+++ forge/src/main/java/io/github/ultreon/mods/essentials/forge/UEssentialsConfigImpl.java
@@ -7,6 +7,6 @@ import net.minecraftforge.fml.config.ModConfig;
 public class UEssentialsConfigImpl {
     public static void init() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, UEssentialsConfig.COMMON_SPEC, "essentials-common.toml");
-        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, UEssentialsConfig.CLIENT_SPEC, "essentials-client.toml"
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, UEssentialsConfig.CLIENT_SPEC, "essentials-client.toml");
     }
 }
